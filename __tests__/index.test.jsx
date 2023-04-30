39e703a8c4d44c4ca13d80dee5b3df17d55ec1c3
gozer <59127276+gohzer@users.noreply.github.com>
2023-04-30 17:47:50 -0400

    add sample test to prevent breaking jest

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,2 +1,9 @@
 // add tests here:
 // refer to https://nextjs.org/docs/testing
+function sum(a, b) {
+    return a + b;
+}
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
