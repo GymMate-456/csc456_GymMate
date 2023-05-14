738529a240a42184b5989cc5a18ffd32f9d75bb1
KHUZIAMA REHMAN <REHMANKHUZIAMA@GMAIL.COM>
2023-05-13 22:21:46 -0400

    fix error with gym card types

--- pages/GymCard.tsx
+++ pages/GymCard.tsx
@@ -1,37 +1,66 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
-//Results for "searched gym" and some information about results displayed in a card format 
+//Results for "searched gym" and some information about results displayed in a card format
 //NOTE: "More Details" button currently doesn't do anything and is just there as a placeholder
+type Photo = {
+  height: number;
+  width: number;
+  photo_reference: string;
+  html_attributions: string[];
+};
 
-interface Gym {
+type GeometryLocation = {
+  lat: number;
+  lng: number;
+};
+
+type GeometryViewport = {
+  northeast: GeometryLocation;
+  southwest: GeometryLocation;
+};
+
+type Geometry = {
+  location: GeometryLocation;
+  viewport: GeometryViewport;
+};
+
+type Gym = {
+  business_status: string;
+  geometry: Geometry;
+  icon: string;
+  icon_background_color: string;
+  icon_mask_base_uri: string;
   name: string;
+  photos?: Photo[];
+  place_id: string;
   rating: number;
+  reference: string;
+  scope: string;
+  types: string[];
+  user_ratings_total: number;
   vicinity: string;
-}
+};
 
-interface GymCardProps {
+type GymCardProps = {
   gym: Gym;
-}
-
+};
 const GymCard: React.FC<GymCardProps> = ({ gym }) => {
-    const { name, rating, vicinity } = gym;
-
   return (
     <Card sx={{ maxWidth: 345, marginBottom: 2 }}>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {name}
+          {gym && gym.name ? gym.name : "Name Not available"}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Rating: {rating}
+          Rating: {gym && gym.rating ? gym.rating : "Not Available"}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Address: {vicinity}
+          Address: {gym && gym.vicinity ? gym.vicinity : "Not Available"}
         </Typography>
       </CardContent>
       <CardActions>
--- pages/searchGyms/index.tsx
+++ pages/searchGyms/index.tsx
@@ -1,68 +1,78 @@
-import { FormEvent, useState, useEffect } from 'react'
-import GymCard from '../GymCard'
+import { FormEvent, useState, useEffect } from "react";
+import GymCard from "../GymCard";
 
 function searchGyms() {
-    const [gymData, setGymData] = useState([])
-    const [enteredLat, setEnteredLat] = useState("")
-    const [enteredLong, setEnteredLong] = useState("")
+  const [gymData, setGymData] = useState([]);
+  const [enteredLat, setEnteredLat] = useState("");
+  const [enteredLong, setEnteredLong] = useState("");
 
-    //Function that grabs user input lat&long and makes API call, searchGyms,to grab gyms nearby given location
-    //then sets "gymData" variable to the response from API call
-    const fetchNearbyGyms = async (latitude: number, longitude: number) => {
-        try {
-          const response = await fetch(`/api/searchGyms?lat=${latitude}&long=${longitude}`);        
-          const gyms = await response.json();
-          setGymData(gyms.results);
-
-        } catch (error) {
-          console.error('Error fetching gyms:', error);
-          return null;
-        }
-    };
-
-    //Function when "Get Nearby Gyms" is pressed, will populate on screen the nearby gyms in a "card" format
-    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-
-        try {
-            const nearbyGyms = await fetchNearbyGyms(Number(enteredLat), Number(enteredLong));
-          } 
-        catch (error) {
-            console.error('Error getting gyms near user:', error);
-          }
-        
+  //Function that grabs user input lat&long and makes API call, searchGyms,to grab gyms nearby given location
+  //then sets "gymData" variable to the response from API call
+  const fetchNearbyGyms = async (latitude: number, longitude: number) => {
+    try {
+      const response = await fetch(
+        `/api/searchGyms?lat=${latitude}&long=${longitude}`
+      );
+      const gyms = await response.json();
+      setGymData(gyms.results);
+    } catch (error) {
+      console.error("Error fetching gyms:", error);
+      return null;
     }
+  };
 
-    //DEV NOTE: To check object(s) of the data that is being grabbed, uncomment next 3 lines and open console in web browser
-    //          and you will be able to see what information is being grabbed from the API.
-    
-    // useEffect(() => {
-    //     console.log("This is our results!",gymData.results)
-    // },[gymData])
-    
-    
-    return(
-        <>
-            <h1>Enter Coordinates to search for gyms nearby! </h1>
+  //Function when "Get Nearby Gyms" is pressed, will populate on screen the nearby gyms in a "card" format
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-            <form onSubmit={handleSubmit}>
-                <label> Latitude: </label>
-                <input type="text" value={enteredLat} onChange={(e) => setEnteredLat(e.target.value)} placeholder='Enter Latitude..'></input>
-                <label> Longitude: </label>
-                <input type="text" value={enteredLong} onChange={(e) => setEnteredLong(e.target.value)} placeholder='Enter Longitude..'></input>
-                <button type="submit">Get Nearby Gyms</button>
-            </form>
-            
-            {/* Note that down below it might show "error" and highlight results/gym/index but the code will still work fine */}
-            { gymData && gymData.map((gym, index) => (
-                <>
-                    <GymCard key={index} gym={gym} />
-                </>
-                ))}
-            
-        </>
-    );
+    try {
+      const nearbyGyms = await fetchNearbyGyms(
+        Number(enteredLat),
+        Number(enteredLong)
+      );
+    } catch (error) {
+      console.error("Error getting gyms near user:", error);
+    }
+  };
 
+  //DEV NOTE: To check object(s) of the data that is being grabbed, uncomment next 3 lines and open console in web browser
+  //          and you will be able to see what information is being grabbed from the API.
+
+  // useEffect(() => {
+  //     console.log("This is our results!",gymData.results)
+  // },[gymData])
+
+  return (
+    <>
+      <h1>Enter Coordinates to search for gyms nearby! </h1>
+
+      <form onSubmit={handleSubmit}>
+        <label> Latitude: </label>
+        <input
+          type="text"
+          value={enteredLat}
+          onChange={(e) => setEnteredLat(e.target.value)}
+          placeholder="Enter Latitude.."
+        ></input>
+        <label> Longitude: </label>
+        <input
+          type="text"
+          value={enteredLong}
+          onChange={(e) => setEnteredLong(e.target.value)}
+          placeholder="Enter Longitude.."
+        ></input>
+        <button type="submit">Get Nearby Gyms</button>
+      </form>
+
+      {/* Note that down below it might show "error" and highlight results/gym/index but the code will still work fine */}
+      {gymData &&
+        gymData.map((gym, index) => (
+          <>
+            <GymCard key={index} gym={gym} />
+          </>
+        ))}
+    </>
+  );
 }
 
 export default searchGyms;
