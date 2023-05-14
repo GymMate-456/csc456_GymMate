d238426c568e9a2a09325c13e19a330928daf5e2
Jeevan Bastola <jeevanbastola986@gmail.com>
2023-05-14 19:58:53 -0400

    location autocomplete added to search gyms

--- pages/searchGyms/index.tsx
+++ pages/searchGyms/index.tsx
@@ -3,11 +3,36 @@ import GymCard from "../GymCard";
 import Header from "../header";
 import styles from "../../styles/profile.module.css";
 import { TextField, Button, CircularProgress, Avatar} from '@mui/material';
+import dynamic from 'next/dynamic';
+import { sendToast } from "../../utils/toasts"
+import { ToastDependency } from "../../utils/toasts"
+
+const LocationAutocomplete = dynamic(() => import('../../utils/LocationAutocomplete'), {
+  ssr: false, // Disable server-side rendering
+});
 
 function searchGyms() {
   const [gymData, setGymData] = useState([]);
   const [enteredLat, setEnteredLat] = useState("");
   const [enteredLong, setEnteredLong] = useState("");
+
+  const handlePlaceSelect = async(coordinates: any) => {
+    // Handle the selected coordinates
+    console.log('Selected coordinates:', coordinates.lat);
+    console.log('Selected coordinates:', coordinates.lng);
+    try {
+      const nearbyGyms = await fetchNearbyGyms(
+        Number(coordinates.lat),
+        Number(coordinates.lng)
+      ).then(async () => {
+        await sendToast("success", "List of gyms loaded successfully.", 500);
+        console.log('List of gym loaded successfully.');
+      })
+    } catch (error) {
+      await sendToast("error", "Error getting gyms in the area:", 500);
+      console.error("Error getting gyms near user:", error);
+    }
+  };
 
   //Function that grabs user input lat&long and makes API call, searchGyms,to grab gyms nearby given location
   //then sets "gymData" variable to the response from API call
@@ -52,32 +77,13 @@ function searchGyms() {
 
       <form onSubmit={handleSubmit}>
         <div className={styles.container}>
-          <div className={styles.heading}>Enter Coordinates to search for gyms nearby! </div>
-          <TextField
-              type="text"
-              label="Latitude"
-              value={enteredLat}
-              onChange={(e) => setEnteredLat(e.target.value)}
-              className={styles.textField}
-          />
-          <TextField
-              type="text"
-              label="Longitude"
-              value={enteredLong}
-              onChange={(e) => setEnteredLong(e.target.value)}
-              className={styles.textField}
-          />
-          <Button
-              type="submit"
-              variant="contained"
-              color="secondary"
-              className={styles.button}
-            >
-              Get Nearby Gyms
-          </Button>
+          <div className={styles.heading} style={{fontSize:'25px'}}>Search for gyms in the area!</div>
+          <div className={styles.textField} style={{margin:'2%'}}>
+            <LocationAutocomplete onSelect={handlePlaceSelect} />
+          </div>
           </div>
       </form>
-    
+    <ToastDependency/>
     <div className={styles.container}>
       
       {/* Note that down below it might show "error" and highlight results/gym/index but the code will still work fine */}
